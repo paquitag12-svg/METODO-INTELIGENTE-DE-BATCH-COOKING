5d36c0afea5e73540d419bd426e07fa5863dfa32
paquitag12-svg <paquitag12@gmail.com>
2026-09-11 10:27:16 -0500

    feat: update recipe banner image
    Replace banner image URL and remove obsolete error handling.

--- src/components/RecipeBannerImage.tsx
+++ src/components/RecipeBannerImage.tsx
@@ -10,15 +10,11 @@ export const RecipeBannerImage: React.FC = () => {
         >
           <img
             id="foto-recetas-banner"
-            src="https://i.imgur.com/9PQZqYT.jpeg"
+            src="https://i.imgur.com/FennOcw.jpeg"
             alt="Muestrario de recetas y preparaciones del método Batch Cooking"
             className="w-full h-auto object-contain max-h-[750px] mx-auto block rounded-2xl group-hover:scale-[1.01] transition-transform duration-500"
             loading="lazy"
             referrerPolicy="no-referrer"
-            onError={(e) => {
-              const target = e.target as HTMLImageElement;
-              target.src = '/assets/recetas-banner-imgur.jpg';
-            }}
           />
         </div>
       </div>
